bcb9ded38672bae0bab5225410e07c793c6d4e89
Leandro Motta <leamotta@msn.com>
2020-04-26 12:49:09 -0300

    Validate url in link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
   validates :code, :destination, presence: true
   validates :code, length: { maximum: 7 }, uniqueness: true
+
+  validates :destination, url: true
 end
